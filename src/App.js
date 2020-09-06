d71b7ee787d81c1d419e91caa364e1c0004e1cff
JonTDean <JonathanTomDean@Gmail.com>
2020-09-06 13:26:25 -0400

    Setting Up React-Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Bootstrap from 'bootstrap';
+import {NavBar, Nav} from 'react-bootstrap'
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="NavBar">
+        <ol>
+          <li>Home</li>
+          <li>Portfolio</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ol>
+      </nav>
     </div>
   );
 }
