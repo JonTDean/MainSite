2e86b44df8c4b4cb39cbe58c16291a2faa8c5349
JonTDean <JonathanTomDean@Gmail.com>
2020-09-08 20:28:33 -0400

    Added the Modal next up is handling empty Inputs

--- src/components/Contact/Alerts/EmailSentAlert.js
+++ src/components/Contact/Alerts/EmailSentAlert.js
@@ -1,36 +1,31 @@
 // Reacto
-import react, { Component, useState } from 'react';
+import React, { Component, useState } from 'react';
 
 // Styling
-import Modal from 'react-bootstrap/Modal'
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 function EmailSentAlert(props){
-    const [show, setShow] = useState(false);
+    const [show, setShow] = useState(true);
 
     const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
   
     return (
-      <>
-        <Button variant="primary" onClick={handleShow}>
-          Launch demo modal
-        </Button>
-  
         <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
+            <Modal.Title>Email Sent!</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Body>
+            Thank you for contacting me, an automated message will be sent to your e-mail and I will return your e-mail shortly after.
+            Thank you, 
+            Jonathan T. Dean
+          </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
               Close
             </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
           </Modal.Footer>
         </Modal>
-      </>
     );
 }
 
--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -10,6 +10,7 @@ import { ReCaptcha } from 'react-recaptcha-google';
 // Components
 import {onLoadRecaptcha, verifyCallback} from './Utils/CaptchaUtils';
 import { sendEmail } from './Utils/FormUtils';
+import EmailSentAlert from './Alerts/EmailSentAlert';
 
 function ContactForm(props){
     // eslint-disable-next-line
@@ -17,6 +18,7 @@ function ContactForm(props){
 
     // Functional States
     const [isVerified, setIsVerified] = useState(false);
+    const [show, setShow] = useState(false);
     const [information, setInformation] = useState({
         from_name: '',
         from_email: '',
@@ -31,43 +33,56 @@ function ContactForm(props){
     }
 
     return(
-        <Form className="w-50 mt-5 container" onSubmit={sendEmail}>
-            <Form.Label className="h3"> Contact Form </Form.Label>
-            <Form.Group as={Row}>                    
-                <Form.Label>Full Name</Form.Label>
-                <Form.Control name="from_name" id="from_name" onChange={handleChange}  value={information.from_name} />
-            </Form.Group>
-
-            <Form.Group as={Row}>       
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" name="from_email" id="from_email" onChange={handleChange} value={information.from_email} />
-            </Form.Group>
-
-            <Form.Group as={Row}>
-                <Form.Label>Message</Form.Label>
-                <Form.Control as="textarea" name="message" id="message" onChange={handleChange} value={information.message} />
-            </Form.Group>
-
-            <Container className="d-flex justify-content-center">
-                {/* This is the Captcha */}
-                <ReCaptcha
-                    ref={(el) => {captcha = el;}}
-                    size="normal"
-                    data-theme="dark"            
-                    render="explicit"
-                    sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
-                    onloadCallback={onLoadRecaptcha} // Captcha is used here
-                    verifyCallback={e => verifyCallback(e, setIsVerified)}
-                />
-
-                {isVerified ?
-                    <Button type="submit">Submit</Button>
-                :
-                    <></>
-                }
+        <>
+            <Form className="w-50 mt-5 container" onSubmit={sendEmail}>
+                <Form.Label className="h3"> Contact Form </Form.Label>
                 
-            </Container>
-        </Form>
+                {/* Form for name */}
+                <Form.Group as={Row}>                    
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control name="from_name" id="from_name" onChange={handleChange}  value={information.from_name} />
+                </Form.Group>
+
+                {/* Form for Email */}
+                <Form.Group as={Row}>       
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" name="from_email" id="from_email" onChange={handleChange} value={information.from_email} />
+                </Form.Group>
+                
+                {/* Form for message */}
+                <Form.Group as={Row}>
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control as="textarea" name="message" id="message" onChange={handleChange} value={information.message} />
+                </Form.Group>
+
+                <Container className="d-flex justify-content-center">
+                    {/* This is the Captcha */}
+                    <ReCaptcha
+                        ref={(el) => {captcha = el;}}
+                        size="normal"
+                        data-theme="dark"            
+                        render="explicit"
+                        sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
+                        onloadCallback={onLoadRecaptcha} // Captcha is used here
+                        verifyCallback={e => verifyCallback(e, setIsVerified)}
+                    />
+
+                    {/*  Only renders button if isVerified is true */}
+                    {/*  Too many re-renders fix https://stackoverflow.com/questions/59304283/error-too-many-re-renders-react-limits-the-number-of-renders-to-prevent-an-in?rq=1 */}
+                    {isVerified ?
+                        <Button type="submit" onClick={() => {setShow(true)}}>Submit</Button>
+                    :
+                        <></>
+                    }
+                </Container>
+            </Form>
+            
+            {show ?
+                <EmailSentAlert />
+            :
+                <></>
+            }
+        </>
     )
 }
 
