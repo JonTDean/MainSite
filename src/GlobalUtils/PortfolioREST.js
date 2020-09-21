a4805dd5e7a70e6c370b0d95131c961e2f3fadb2
JonTDean <JonathanTomDean@Gmail.com>
2020-09-21 17:00:03 -0400

    PortfolioREST

---
+++ src/GlobalUtils/PortfolioREST.js
@@ -0,0 +1,8 @@
+const Github_API = [
+    'https://api.github.com/repos/JonTDean/EoE',
+    '/JonTDean/Soulys_Frontend',
+    ''
+]
+function filterGithub(){
+
+}
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,6 +1,12 @@
 // Reacto
 import React, {Component} from 'react';
+
+// Styling
 import { motion } from 'framer-motion';
+import { Container, Carousel } from 'react-bootstrap';
+
+// Libraries
+// Components
 
 class Portfolio extends Component{
     render(){
@@ -13,9 +19,23 @@ class Portfolio extends Component{
                 exit="out"
                 variants={this.props.pageVariant}
             >
-                <p>PORTFOLIO PAGE!</p>
-                <p>Link Github Account Here</p>
-                <p>Also have your portfolio show off your work directly in the browser, maybe via {"<Iframes>"}?</p>
+                <Container className="mt-5" style={{backgroundColor: "black"}}>
+
+                    <Carousel>
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100 h-100"
+                            // src="holder.js/800x400?text=First slide&bg=373940"
+                            src='https://i.guim.co.uk/img/media/d143e03bccd1150ef52b8b6abd7f3e46885ea1b3/0_182_5472_3283/master/5472.jpg?width=1200&quality=85&auto=format&fit=max&s=d5a74a011c3fef1ad9c1c962721d221d'
+                            // alt="First slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>First slide label</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                </Container>
             </motion.div>
 
         )
